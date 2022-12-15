461d972c345031738d71a502a18a4e266b1822b4
Pavel Yastremsky <goodieart@gmail.com>
2022-12-15 16:25:45 +0300

    task_002 refactored

--- task_002/main.py
+++ task_002/main.py
@@ -4,11 +4,10 @@
 from math import ceil
 
 user_input = list(map(int, input('Введите числа через пробел: ').split()))
-muls = []
+r = user_input.copy()
+r.reverse()
 center = ceil(len(user_input) / 2)
-
-for i in range(center):
-    muls.append(user_input[i] * user_input[-i-1])
+muls = [a*b for a,b in zip(user_input, r)][:center]
 
 print(f'Результирующий список: {user_input}')
 print(f'Произведение пар чисел списка: {muls}')
